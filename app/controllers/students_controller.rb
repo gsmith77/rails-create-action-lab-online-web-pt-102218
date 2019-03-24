464ed8b424fbdef787058571a9eda4bc5f9efc0a
grant smith <grantsmith1256@gmail.com>
2019-03-24 03:35:57 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    binding.pry
     @student = Student.new
     @student.first_name = params[:student][:first_name]
     @student.last_name = params[:student][:last_name]
